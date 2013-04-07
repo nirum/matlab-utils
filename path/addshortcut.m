a7d139abe421bc682281daad769d0c86d1d337f8
Niru Maheswaranathan <nirum@stanford.edu>
2013-04-06 17:25:18 -0700

    fixed bug in shortcuts script; adding shorcuts.mat to repo

--- path/addshortcut.m
+++ path/addshortcut.m
@@ -6,4 +6,4 @@
 function addshortcut(str)
 eval([str ' = pwd;']);
 fprintf(['Saved ' pwd ' as shortcut ' str '.\n']);
-eval(['save(''~/code/toolbox/matlab-utils.mat'', ''' str ''', ''-append'');']);
+eval(['save(''~/code/toolbox/matlab-utils/shortcuts.mat'', ''' str ''', ''-append'');']);
